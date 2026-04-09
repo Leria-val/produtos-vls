cf8d3345c04ec966cdcc8a14d6bd153bb2b20aca
leonardobellorin568-cmd <leonardobellorin568@gmail.com>
2026-04-09 11:14:37 -0300

    User service

--- frontend/produtos-vls/src/api/userService.jsx
+++ frontend/produtos-vls/src/api/userService.jsx
@@ -1,1 +1,16 @@
-// valerina capuchina
+import apiClient from "./axiosConfig";
+
+export const loginUser = async (credentials) => {
+    const response = await apiClient.post('/auth/login', credentials);
+    return response.data;
+};
+
+export const registerUser = async (userData) => {
+    const response = await apiClient.post('/auth/register', userData);
+    return response.data;
+};
+
+export const getProfile = async () => {
+    const response = await apiClient.get('/auth/profile');
+    return response.data;
+};
